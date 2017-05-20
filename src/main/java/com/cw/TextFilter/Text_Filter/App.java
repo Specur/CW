559e84c06cbc81e4da56f511c4121b6b0ca634e4
Specur <mmmstec@o2.pl>
2017-05-20 16:15:19 +0200

    initial commit

---
+++ src/main/java/com/cw/TextFilter/Text_Filter/App.java
@@ -0,0 +1,13 @@
+package com.cw.TextFilter.Text_Filter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
